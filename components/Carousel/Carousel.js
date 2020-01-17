aafefede8003e75b9fd390f4e1387d2db9ae3cfd
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-01-17 12:16:29 +0000

    test

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -6,7 +6,6 @@
     5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
-
 /* HTML:
   <div class="carousel">
     <div class="left-button"> < </div>
