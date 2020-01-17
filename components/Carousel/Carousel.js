27fe482f9cc7e25755ff2212b82b9ced4b9fa230
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-01-17 12:45:51 +0000

    added carousel component start

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,4 @@
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to write a function that creates the carousel component, you will find the HTML below.
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
@@ -16,3 +15,50 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+// 1. You will need to write a function that creates the carousel component, you will find the HTML below.
+
+
+function carouselComponent(){
+
+const carousel = document.createElement('div');
+const buttonleft = document.createElement('div');
+const img1 = document.createElement('img');
+const img2 = document.createElement('img');
+const img3 = document.createElement('img');
+const img4 = document.createElement('img');
+const buttonright = document.createElement('div');
+
+//apending
+
+carousel.appendChild(buttonleft);
+carousel.appendChild(buttonright);
+carousel.appendChild(img1);
+carousel.appendChild(img2);
+carousel.appendChild(img3);
+carousel.appendChild(img4);
+
+//class
+
+carousel.classList.add('carousel');
+buttonleft.classList.add('left-button');
+buttonright.classList.add('right-button');
+
+//content 
+
+// buttonleft.textContent = '\u25BA';
+// buttonright.textContent = '\u25BC';
+
+//add img.src to content 
+
+return carousel;
+
+}
+
+
+//select parent container 
+const carouselContainer = document.querySelector(".carousel-container");
+
+// Appending 
+
+carouselContainer.append(carouselComponent());
